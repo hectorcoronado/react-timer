8d1cc977b6e5a955940b2d4e7d367eb86abf7999
hectorcoronado <coronado.hector@gmail.com>
2016-09-11 16:37:30 -0700

    Navigation component added

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,8 +1,11 @@
 var React = require('react');
 
+var Navigation = require('Navigation');
+
 var Main = (props) => {
   return (
   <div>
+    <Navigation/>
     <div>
       <p>Main.jsx Rendered</p>
       {props.children}
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,38 @@
+var React = require('react'),
+    {Link, IndexLink} = require('react-router');
+
+var Navigation = React.createClass({
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              React Timer
+            </li>
+            <li>
+              <IndexLink to='/' activeClassName="active-link">
+                Timer
+              </IndexLink>
+            </li>
+            <li>
+              <Link to='/' activeClassName="active-link">
+                Countdown
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="http://www.hectorcoronado.codes" target="_blank">Hector Coronado</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+
+});
+
+module.exports = Navigation;
