bcb6fd9784e2a987aa39e02e8c54fd08f507bbe9
hectorcoronado <coronado.hector@gmail.com>
2016-09-13 22:31:20 -0700

    Countdown.jsx & Timer.jsx components added & wired up to Main.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,9 @@ var React = require('react'),
     ReactDOM = require('react-dom'),
     {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-var Main = require('Main');
+var Main = require('Main'),
+    Timer = require('Timer'),
+    Countdown = require('Countdown');
 
 // Load Foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
@@ -15,7 +17,8 @@ require('style!css!sass!applicationStyles')
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
-      
+      <Route path='countdown' component={Countdown}/>
+      <IndexRoute component={Timer}/>
     </Route>
   </Router>,
   document.getElementById('app')
--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -16,7 +16,7 @@ var Navigation = React.createClass({
               </IndexLink>
             </li>
             <li>
-              <Link to='/' activeClassName="active-link">
+              <Link to='/countdown' activeClassName="active-link">
                 Countdown
               </Link>
             </li>
